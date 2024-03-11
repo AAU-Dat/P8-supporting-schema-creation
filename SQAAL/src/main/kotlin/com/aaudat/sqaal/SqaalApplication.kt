8cfefb0cf20fa9a815614ec44b0714ac5ff8f2c9
Daniel Runge Petersen <dpet20@student.aau.dk>
2024-03-11 23:04:51 +0100

    Create SQAAL as Spring Shell project

---
+++ SQAAL/src/main/kotlin/com/aaudat/sqaal/SqaalApplication.kt
@@ -0,0 +1,11 @@
+package com.aaudat.sqaal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SqaalApplication
+
+fun main(args: Array<String>) {
+    runApplication<SqaalApplication>(*args)
+}
---
+++ SQAAL/src/test/kotlin/com/aaudat/sqaal/SqaalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aaudat.sqaal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SqaalApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
