b2ab229543e79aa21fe6bf08892cc69ee78c5ba6
Anders Malta <74375517+Maltesius@users.noreply.github.com>
2024-03-14 14:52:17 +0100

    Add library (#80)
    * Added DFA library from Anders Møller (AU)
    
    * Imported DFA library from Anders Møller (AU)
    
    * Imported DFA library from Anders Møller (AU)

--- SQAAL/src/main/kotlin/com/aaudat/sqaal/SqaalApplication.kt
+++ SQAAL/src/main/kotlin/com/aaudat/sqaal/SqaalApplication.kt
@@ -2,6 +2,7 @@ package com.aaudat.sqaal
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import dk.brics.automaton.*
 
 @SpringBootApplication
 class SqaalApplication
