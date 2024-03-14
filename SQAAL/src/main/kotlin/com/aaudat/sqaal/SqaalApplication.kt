128f390c0253116f82243691bd5ad32da5272a34
Maltesius <andersmalta@gmail.com>
2024-03-14 14:47:11 +0100

    Imported DFA library from Anders Møller (AU)

--- SQAAL/src/main/kotlin/com/aaudat/sqaal/SqaalApplication.kt
+++ SQAAL/src/main/kotlin/com/aaudat/sqaal/SqaalApplication.kt
@@ -2,10 +2,10 @@ package com.aaudat.sqaal
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import dk.brics.automaton.*
 
 @SpringBootApplication
 class SqaalApplication
 
 fun main(args: Array<String>) {
-    runApplication<SqaalApplication>(*args)
 }
