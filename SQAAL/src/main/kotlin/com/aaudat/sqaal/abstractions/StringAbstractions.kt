cbe5a9d0d951b33a84e6d6f6f0d49562fee23387
Maltesius <andersmalta@gmail.com>
2024-03-20 15:03:15 +0100

    Added some functions, making it easier to work with the library

--- SQAAL/src/main/kotlin/com/aaudat/sqaal/abstractions/StringAbstractions.kt
+++ SQAAL/src/main/kotlin/com/aaudat/sqaal/abstractions/StringAbstractions.kt
@@ -1,5 +1,7 @@
 package com.aaudat.sqaal.abstractions
 import dk.brics.automaton.Automaton
+import dk.brics.automaton.RegExp
+import net.sf.jsqlparser.statement.Statement
 
 /**
  * Class for handling the abstractions of String values
@@ -11,6 +13,27 @@ import dk.brics.automaton.Automaton
  * - [dk.brics.automaton]
  */
 abstract class StringAbstractions {
+
+    /**
+     * Function for creating a [RegExp] from a [String]
+     * @param parsedast todo: Unsure what to give at the moment. Potential types could be [String] and [Statement]
+     * @return [RegExp]
+     */
+    private fun toRegExp(parsedast: String): RegExp {
+        val re = RegExp(parsedast);
+        return re
+        TODO("Convert ??? (maybe parsed SQL AST or String) to Regular Expression")
+    }
+
+    /**
+     * Function for creating an [Automaton] from a [RegExp]
+     * @param re [RegExp]: Used for creating the [Automaton]
+     * @return [Automaton]: The created [Automaton]
+     */
+    private fun toAutomata(re: RegExp): Automaton {
+        val automaton = re.toAutomaton()
+        return automaton
+    }
 
     /**
      * Function for checking whether the languages of two automatas are equal
