b429bc320b6f7d43545d0d70ed0fb29937c266d4
Sebastian <72598707+sebastianbot6969@users.noreply.github.com>
2024-03-25 14:01:17 +0100

    Delete SQAAL/src/main/resources/sqlExamples/delete.sql

--- SQAAL/src/main/resources/sqlExamples/delete.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM orders
-WHERE EXISTS
-          (SELECT *
-           FROM customers
-           WHERE customers.customer_id = orders.customer_id
-             AND customers.last_name = 'Jackson');
