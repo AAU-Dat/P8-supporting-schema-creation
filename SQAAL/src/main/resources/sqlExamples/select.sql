38b1c0909bdb9bf41e7cb4733d53c52024ea93d3
Sebastian <72598707+sebastianbot6969@users.noreply.github.com>
2024-03-25 14:01:58 +0100

    Delete SQAAL/src/main/resources/sqlExamples/select.sql

--- SQAAL/src/main/resources/sqlExamples/select.sql
+++
@@ -1,9 +0,0 @@
-SELECT /*+ PARALLEL */
-    cfe.id_collateral_ref.nextval
-     , id_collateral
-FROM (  SELECT DISTINCT
-            a.id_collateral
-        FROM cfe.collateral a
-                 LEFT JOIN cfe.collateral_ref b
-                           ON a.id_collateral = b.id_collateral
-        WHERE b.id_collateral_ref IS NULL )
