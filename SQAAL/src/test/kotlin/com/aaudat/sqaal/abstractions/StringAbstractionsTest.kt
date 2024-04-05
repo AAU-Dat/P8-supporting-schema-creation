0c6b25e449138ec47184b735294cc7a20ae7157c
Maltesius <andersmalta@gmail.com>
2024-04-05 11:48:24 +0200

    Made tests for cover lattice function

---
+++ SQAAL/src/test/kotlin/com/aaudat/sqaal/abstractions/StringAbstractionsTest.kt
@@ -0,0 +1,26 @@
+package com.aaudat.sqaal.abstractions
+
+import com.aaudat.sqaal.Commands
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class StringAbstractionsTest {
+    private val testStringAbstractions: StringAbstractions = StringAbstractions()
+
+    @Test
+    fun testNoSubset() {
+        assertThrows(IndexOutOfBoundsException::class.java) {
+            testStringAbstractions.belongs("cc","a* b* ab(c|d)*")
+        }
+    }
+
+    @Test
+    fun testSubset() {
+        testStringAbstractions.belongs("abcd", "a* b* ab(c|d)*")
+
+        testStringAbstractions.belongs("a", "a* b* ab(c|d)*")
+
+        testStringAbstractions.belongs("b", "a* b* ab(c|d)*")
+    }
+}
